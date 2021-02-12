9538b49ee012cf09de342d485de53cafc4c897d5
jjgarciac <jjgarcia@cs.unc.edu>
2021-02-12 11:09:17 -0500

    Initial commit.

---
+++ mixup.py
@@ -0,0 +1,194 @@
+from sklearn.neighbors import NearestNeighbors
+import tensorflow.keras as keras
+import numpy as np
+
+
+def mixup_uncertainty(batch_x, batch_y, x, y, alpha=1.0, out_of_class=False,
+        local=True, manifold_mixup=False):
+    
+    batch_size = batch_x.shape[0]
+    
+    if alpha > 0:
+        lam = np.random.beta(alpha, alpha, batch_size)
+    else:
+        lam = np.ones(batch_size)
+    rlam = lam.reshape((batch_size, 1))
+    
+    if local:
+      index = np.random.permutation(batch_size)
+      mixed_x = rlam * batch_x + (1 - rlam) * batch_x[index, :]
+      mixed_y = rlam * batch_y + (1 - rlam) * batch_y[index, :]
+      input_x  = [batch_x, batch_x[index, :], rlam] if manifold_mixup else mixed_x
+    else:
+      index = np.random.permutation(x.shape[0])[:batch_size]
+      mixed_x = rlam * batch_x + (1 - rlam) * x[index, :]
+      mixed_y = rlam * batch_y + (1 - rlam) * y[index, :]
+      input_x  = [batch_x, x[index, :], rlam] if manifold_mixup else mixed_x
+
+    return input_x, mixed_y
+
+def mixup_random(batch_x, batch_y, x, y, alpha=1.0, out_of_class=False,
+                 local=True, manifold_mixup=False):
+    batch_size = batch_x.shape[0]
+
+    if alpha > 0:
+        lam = np.random.beta(alpha, alpha, batch_size)
+    else:
+        lam = np.ones(batch_size)
+    rlam = lam.reshape((batch_size, 1))
+    
+    if local:
+      index = np.random.permutation(batch_size)
+      mixed_x = rlam * batch_x + (1 - rlam) * batch_x[index, :]
+      mixed_y = rlam * batch_y + (1 - rlam) * batch_y[index, :]
+      input_x  = [batch_x, batch_x[index, :], rlam] if manifold_mixup else mixed_x
+    else:
+      index = np.random.permutation(x.shape[0])[:batch_size]
+      mixed_x = rlam * batch_x + (1 - rlam) * x[index, :]
+      mixed_y = rlam * batch_y + (1 - rlam) * y[index, :]
+      input_x  = [batch_x, x[index, :], rlam] if manifold_mixup else mixed_x
+
+    return input_x, mixed_y
+
+
+def knn(x, k=None):
+    """Find k-nearest neighbor of each sample"""
+    
+    if k is None:
+      k = x.shape[0]
+    neigh = NearestNeighbors(n_neighbors=k, n_jobs=-1).fit(x)
+    return neigh
+
+def mixup_knn(batch_x, batch_y, x, y, neigh, k, alpha=1.0, nearest=True,
+              out_of_class=False, manifold_mixup=False):
+    batch_size = batch_x.shape[0]
+
+    if alpha > 0:
+        lam = np.random.beta(alpha, alpha, batch_size)
+    else:
+        lam = np.ones(batch_size)
+    rlam = lam.reshape((batch_size, 1))
+        
+    indices = neigh.kneighbors(batch_x, return_distance=False)
+
+    if out_of_class:
+      selected_indices = []
+      for x_indices, y_label in zip(indices, batch_y):
+        n = 0
+        if not nearest:
+          x_indices = x_indices[::-1]
+        tmp = []
+        for i in x_indices:
+          if not np.equal(y[i], y_label).all():
+            tmp.append(i)
+            n += 1
+            if n >= k:
+                break
+            
+        selected_indices.append(tmp)
+      selected_indices = np.array(selected_indices)
+    else:
+      if nearest:
+        selected_indices = indices[:,:k]
+      else:
+        selected_indices = indices[:, -1*k:]
+
+    indices_t = selected_indices.T
+    np.random.shuffle(indices_t)
+    shuffle_indices = indices_t.T
+    index = shuffle_indices[:, 0]
+    
+    mixed_x = rlam * batch_x + (1 - rlam) * x[index, :]
+    mixed_y = rlam * batch_y + (1 - rlam) * y[index, :]
+
+    input_x  = [batch_x, x[index, :], rlam] if manifold_mixup else mixed_x
+
+    return input_x, mixed_y
+
+
+def mixup(batch_x, batch_y, x, y, k, scheme, alpha, local, out_of_class, manifold_mixup, neigh):
+  if scheme in ['random', 'none']:
+    return mixup_random(batch_x, batch_y, x, y, alpha=alpha, out_of_class=out_of_class,
+                        local=local, manifold_mixup=manifold_mixup)
+  nearest = True if scheme == 'knn' else False
+  return mixup_knn(batch_x, batch_y, x, y, neigh, k=k, alpha=alpha,
+                   nearest=nearest,out_of_class=out_of_class, manifold_mixup=manifold_mixup)
+
+class data_generator(keras.utils.Sequence):
+    'Generates data for Keras'
+    def __init__(self, 
+                 x, 
+                 y, 
+                 batch_size=32, 
+                 n_channels=1,
+                 shuffle=True,
+                 mixup_scheme='random',
+                 k=None,
+                 alpha=1.0,
+                 local=False,
+                 out_of_class=False,
+                 manifold_mixup=False
+                 ):
+        'Initialization'
+        
+        self.x = x
+        self.y = y
+        self.n_samples = x.shape[0]
+        self.batch_size = batch_size
+        self.n_channels = n_channels
+
+        self.dim = (x.shape[1], x.shape[1], x.shape[1])
+        self.shuffle = shuffle
+        self.mixup_scheme = mixup_scheme
+        self.neigh = None
+        self.k = k
+        self.alpha = alpha
+        self.local = local
+        self.out_of_class = out_of_class
+        self.manifold_mixup = manifold_mixup
+
+        self.indexes = np.arange(self.n_samples)
+        self.neigh = knn(x) if mixup_scheme=='knn' else None
+        if self.shuffle:
+          np.random.shuffle(self.indexes)
+        self.on_epoch_end()
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.floor(self.n_samples / self.batch_size))
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        # Generate data
+        x, y = self.__data_generation(indexes)
+
+        return x, y
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(self.n_samples)
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, batch_indices):
+        'Generates data containing batch_size samples' 
+
+        batch_x = self.x[batch_indices]
+        batch_y = self.y[batch_indices]
+        x, y = mixup(batch_x=batch_x, 
+                      batch_y=batch_y, 
+                      x=self.x, 
+                      y=self.y, 
+                      k=self.k, 
+                      scheme=self.mixup_scheme, 
+                      alpha=self.alpha, 
+                      local=self.local, 
+                      out_of_class=self.out_of_class,
+                      manifold_mixup=self.manifold_mixup,
+                      neigh=self.neigh
+                     )
+
+        return x, y
---
+++ models.py
@@ -0,0 +1,64 @@
+import tensorflow as tf
+import tensorflow.keras as tfk
+from tensorflow.keras.layers import Dense
+import math
+
+def gauss_pdf(x, name=None):
+    return (1/tf.sqrt(2*math.pi))*tf.math.exp((-tf.pow(x, 2))/2)
+
+class ManifoldMixup(tf.keras.Model):
+
+    def __init__(self, hidden_layers=[16,2,16], output_shape=2):
+        super(ManifoldMixup, self).__init__()
+        
+        for i, nodes in enumerate(hidden_layers):
+            setattr(self, 'dense{}'.format(i+1),
+                    tf.keras.layers.Dense(nodes, activation=tf.nn.relu))
+        
+        self.final = tf.keras.layers.Dense(output_shape, activation="linear")
+    
+    def call(self, inputs, training=False):
+        x1, x2, lam = inputs
+        
+        if training:
+            k = tf.random.uniform((1,), minval=0, maxval=len(self.layers),
+                                dtype=tf.dtypes.int32, name="manifold_idx")[0]
+        else:
+            k = 0.0
+            lam = 1.0
+        x = x1
+        if k == 0:
+            # Denotes data mixup
+            x = lam*x1 + (1-lam)*x2
+        
+        mix = True
+        for i,l in enumerate(self.layers):
+            if (i+1) > k: mix=False
+            if mix:
+                x1 = l(x1)
+                x2 = l(x2)
+                # Keep a copy of the mixed-up manifold
+                x = lam*x1 + (1-lam)*x2
+            else: # Proceed on mixed batch
+                x = l(x)
+        return x
+
+def build_model(in_shape, out_shape, manifold_mixup=False):
+
+  if manifold_mixup:
+    model = ManifoldMixup(hidden_layers=[64,64,64,64], output_shape=out_shape)
+  else:
+    model = tfk.Sequential([
+        Dense(128, activation='relu', input_shape=(in_shape,)),
+        Dense(128, activation='relu'),
+        Dense(out_shape)
+    ])
+
+  optimizer = tfk.optimizers.Adam()
+  loss = tfk.losses.CategoricalCrossentropy(from_logits=True)
+  metrics = [tfk.metrics.CategoricalAccuracy('accuracy', dtype=tf.float32)]
+  model.compile(optimizer=optimizer, 
+                loss=loss, 
+                metrics=metrics)
+  
+  return model
